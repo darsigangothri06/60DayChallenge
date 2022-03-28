8216702f68201fa953fb014be1af4a82dc80e0ec
darsigangothri06 <darsigangothri@gmail.com>
2022-03-28 22:30:04 +0530

    Day 19 Solutions

---
+++ 19. Cells in a Range on an Excel Sheet/Method-1.py
@@ -0,0 +1,1 @@
+return [chr(j) + str(i) for j in range(ord(s[0]), ord(s[3]) + 1) for i in range(int(s[1]), int(s[4])+1)] 
---
+++ 19. Keep Multiplying Found Values by Two/Method-1.py
@@ -0,0 +1,5 @@
+while True:
+    if original in nums:
+        original = original * 2
+    else:
+        return original
